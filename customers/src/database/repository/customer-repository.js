530eed2b51cc8989026bfa30dfa67aa350cabc70
Shelunagori <shelunagori@gmail.com>
2024-04-28 14:39:11 +0530

    updating all customer services

--- customers/src/database/repository/customer-repository.js
+++ customers/src/database/repository/customer-repository.js
@@ -69,11 +69,7 @@ class CustomerRepository {
 
   async FindCustomerById({ id }) {
     try {
-      const existingCustomer = await CustomerModel.findById(id)
-        .populate("address")
-        .populate("wishlist")
-        .populate("orders")
-        .populate("cart.product");
+      const existingCustomer = await CustomerModel.findById(id).populate("address");
       return existingCustomer;
     } catch (err) {
       throw new APIError(
@@ -100,8 +96,13 @@ class CustomerRepository {
     }
   }
 
-  async AddWishlistItem(customerId, product) {
+  async AddWishlistItem(customerId, {_id, name, banner, price, available, description}) {
     try {
+
+      const product  = {
+        _id, name, banner, price, available, description
+      }
+
       const profile = await CustomerModel.findById(customerId).populate(
         "wishlist"
       );
@@ -141,15 +142,15 @@ class CustomerRepository {
     }
   }
 
-  async AddCartItem(customerId, product, qty, isRemove) {
+  async AddCartItem(customerId, {_id, name, banner, price}, qty, isRemove) {
     try {
       const profile = await CustomerModel.findById(customerId).populate(
-        "cart.product"
+        "cart"
       );
 
       if (profile) {
         const cartItem = {
-          product,
+          product: {_id, name, banner, price},
           unit: qty,
         };
 
@@ -179,7 +180,7 @@ class CustomerRepository {
 
         const cartSaveResult = await profile.save();
 
-        return cartSaveResult.cart;
+        return cartSaveResult;
       }
 
       throw new Error("Unable to add to cart!");
